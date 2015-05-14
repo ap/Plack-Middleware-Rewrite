00cb8537175c31fb677b7e25a27a33e9c5cff8f2
Aristotle Pagaltzis <pagaltzis@gmx.de>
2015-05-14 16:16:03 +0200

    redesign the entire interface

--- lib/Plack/Middleware/Rewrite.pm
+++ lib/Plack/Middleware/Rewrite.pm
@@ -8,27 +8,46 @@ package Plack::Middleware::Rewrite;
 
 use parent 'Plack::Middleware';
 
-use Plack::Util::Accessor 'rules';
+use Plack::Util::Accessor qw( request response rules );
 use Plack::Request ();
 use Plack::Util ();
+use overload ();
 
 sub call {
 	my $self = shift;
 	my ( $env ) = @_;
 
-	my ( $app, $res ) = $self->app;
-	my ( $rules, $modify_cb ) = $self->rules;
+	my ( $app, $res, $legacy );
+	my ( $rules, $modify_cb ) = ( $self->request, $self->response );
+
+	unless ( $rules or $modify_cb ) {
+		$rules = $self->rules;
+		$legacy = 1;
+	}
 
 	# call rules with $_ aliased to PATH_INFO
-	( $res ) = map { scalar $rules->( $env ) } $env->{'PATH_INFO'};
+	( $res ) = map { scalar $rules->( $env ) } $env->{'PATH_INFO'}
+		if $rules;
 
-	# upgrade scalar return value, but only if it looks like an HTTP status
-	$res = [ $res, [], [] ]
-		if not ref $res
-		and defined $res
-		and $res =~ /\A[1-5][0-9][0-9]\z/;
+	if ( $legacy ) {
+		if    ( 'CODE'  eq ref $res ) { ( $modify_cb, $res ) = $res }
+		elsif ( 'ARRAY' eq ref $res ) { undef $res if not @$res }
+		elsif ( ref $res )            { undef $res }
+		else {
+			# upgrade scalar to response if it looks like an HTTP status
+			$res = ( defined $res and $res =~ /\A[1-5][0-9][0-9]\z/ )
+				? [ $res, [], [] ]
+				: undef;
+		}
+	}
+	else {
+		if    ( 'CODE'  eq ref $res ) { ( $app, $res ) = $res }
+		elsif ( 'ARRAY' eq ref $res ) { @$res = ( 303, [], [] ) if not @$res }
+		elsif ( ref $res )            { die 'Unhandled reference type in request rewrite: ', overload::StrVal( $res ), "\n" }
+		else                          { undef $res }
+	}
 
-	if ( 'ARRAY' eq ref $res and @$res ) { # external redirect, or explicit response
+	if ( $res ) { # external redirect, or explicit response
 		push @$res, map { [] } @$res .. 2;
 
 		if ( $res->[0] =~ /\A3[0-9][0-9]\z/ ) {
@@ -49,7 +68,7 @@ sub call {
 		}
 	}
 	else { # internal redirect
-		$modify_cb = $res if 'CODE' eq ref $res;
+		$app ||= $self->app;
 		$res = $app->( $env );
 	}
 
@@ -71,25 +90,25 @@ __END__
  use Plack::Builder;
  
  builder {
-     enable 'Rewrite', rules => sub {
+     enable 'Rewrite', request => sub {
          s{^/here(?=/|$)}{/there};
 
-         return 303
+         return [303]
              if s{^/foo/?$}{/bar/}
              or s{^/baz/?$}{/quux/};
 
          return [301, [ Location => 'http://example.org/' ], []]
              if m{^/example/?$};
 
-         return 201 if $_ eq '/favicon.ico';
+         return [201] if $_ eq '/favicon.ico';
 
-         return 503 if -e '/path/to/app/maintenance.lock';
+         return [503] if -e '/path/to/app/maintenance.lock';
 
          return [200, [qw(Content-Type text/plain)], ['You found it!']]
              if $_ eq '/easter-egg';
-
-         return sub { $_->set( 'Content-Type', 'application/xhtml+xml' ) }
-             if ( $_[0]{'HTTP_ACCEPT'} || '' ) =~ m{application/xhtml\+xml(?!\s*;\s*q=0)}
+     }, response => sub {
+         $_->set( 'Content-Type', 'application/xhtml+xml' )
+             if ( $_[0]{'HTTP_ACCEPT'} || '' ) =~ m{application/xhtml\+xml(?!\s*;\s*q=0)};
      };
      $app;
  };
@@ -102,66 +121,71 @@ be done with mod_rewrite can be done with this middleware much more intuitively
 (if in syntactically wordier ways). Its primary purpose is rewriting paths, but
 almost anything is possible very easily.
 
-=head1 CONFIGURATIONS
+=head1 CONFIGURATION OPTIONS
+
+=head2 C<request>
+
+Takes a reference to a function that will be called in scalar context for each
+request. On call, C<$_> will be aliased to C<PATH_INFO>, so that you can easily
+use regexp matches and subtitutions to examine and modify it. The L<PSGI>
+environment will be passed to the function as its first and only argument.
+
+The function may return three kinds of valid value:
 
 =over 4
 
-=item rules
+=item A plain scalar
 
-C<rules> takes a reference to a function that will be called in scalar context
-for each request. During the call, C<PATH_INFO> is aliased to C<$_>, so that
-you can easily use regexp matches and subtitutions to examine and modify it.
-The L<PSGI> environment will be passed as its first and only argument. The
-function can return four (and a half) kinds of values:
+Ignored. The value will be thrown away and any path rewriting (or any other
+modifications of the PSGI environment) will take effect during the current
+request cycle, invisibly to the client user agent.
 
-=over 4
+=item An array reference
 
-=item C<undef>
+A L<PSGI> array response to return immediately without invoking the wrapped
+PSGI application.
 
-Any path and query string rewriting will be treated as an internal rewrite,
-invisible to the user. This is just like having C<RewriteRule>s that do not
-redirect.
+The array may have fewer than 3 elements, in which case it will be filled to
+3E<nbsp>elements by pushing the default values: an empty body array, empty
+headers array, and a 303E<nbsp>statusE<nbsp>code.
 
-=item A scalar value that looks like an HTTP status
-
-This will stop the request from being processed further. An empty response with
-the returned status will be sent to the browser. If it is a redirect status,
-then the rewritten C<PATH_INFO> will be used as the redirect destination.
-
-Note that this means you can only redirect to other URLs on the same domain.
-To redirect to another host entirely, you will need to supply a C<Location>
-header manually, which requires returning either an array or code reference.
-
-=item An array reference (with at least one element)
-
-This is assumed to be a regular L<PSGI> response, except that you may omit
-either or both the headers and body elements. Empty ones will be supplied for
-you, for convenience. You I<may not> omit the status code.
+If the C<Location> header is missing from a redirect response (i.e. one with
+3xxE<nbsp>statusE<nbsp>code), it will be filled in automatically from the value
+left in C<PATH_INFO> by your callback. (Note that this only allows you to
+redirect to URLs with the same hostname. To redirect the client to a different
+host, you will have to supply a C<Location> header manually.)
 
 =item A code reference
 
-The function you supply will be called I<after> the request has been processed,
-with C<$_> aliased to a L<C<Plack::Util::headers>|Plack::Util> object for the
-response, for convenient alteration of headers. The L<PSGI> environment is,
-again, passed as its first and only argument.
+A PSGI application which will be called to process the request. This prevents
+the wrapped application from being called.
 
-Any return value from this function will be ignored E<mdash>
+=item Any other kind of reference
 
-E<hellip> except if this function returns a function in turn. In that case, the
-function returned will be taken to be a filter for the body of the response
-about to be generated, with the interface documented in
-L<Plack::Middleware/RESPONSE CALLBACK>:
+Error. An exception will be thrown.
+
+=back
+
+=head2 C<response>
+
+Takes a reference to a function that will be called I<after> the request has
+been processed and the response is ready to be returned.
+
+On call, C<$_> will be aliased to a L<C<Plack::Util::headers>|Plack::Util>
+object for the response, for convenient alteration of headers. Just as in
+L</C<request>>, the L<PSGI> environment is passed as first and only argument.
+
+Any return value from this function will be ignored unless it is a code
+reference. In that case it will be used to filter the response body, as
+documented in L<Plack::Middleware/RESPONSE CALLBACK>:
 
 =over 4
 
  return sub {
-     my $res = shift;
-     return sub {
-         my $chunk = shift;
-         return unless defined $chunk;
-         $chunk =~ s/Foo/Bar/g;
-         return $chunk;
-     }
+     my $chunk = shift;
+     return unless defined $chunk;
+     $chunk =~ s/Foo/Bar/g;
+     return $chunk;
  };
 
 The callback takes one argument C<$chunk> and your callback is expected to
@@ -171,16 +195,68 @@ final chunk and return undef when called next time.
 
 =back
 
-=item Any other kind of value
+=head1 LEGACY INTERFACE
 
-Other values are currently treated the same as returning nothing. This may
-change in the future, depending on whether ambiguities crop up in practice.
-If you want to be absolutely certain to avoid ambiguities, return one-element
-arrays instead of plain values, and use an explicit C<return> at the end of
-your rules:
+The old interface uses a single attribute, C<rules>, instead of the C<request>
+and C<response> pair, with a more complex set of return values, containing an
+ambiguity. It is also less expressive than the new interface.
+
+The old interface is documented here for the purposes of maintaining old code;
+its use in new code is L<discouraged|perlpolicy/Terminology>. In the far future
+it may get removed entirely, and in the meantime it will not gain new features.
+
+The return value of the C<rules> callback is interpreted as follows:
+
+=over 4
+
+=item An array reference (with at least one element)
+
+A regular L<PSGI> response, except that you may omit either or both the headers
+and body elements. You I<may not> omit the status.
+
+=item A scalar value that looks like an HTTP status
+
+Like returning a reference to a one-element.
+
+Beware: every subroutine in Perl has a return value, even if you do not return
+anything explicitly. To avoid ambiguities you must return one-element arrays
+instead of plain values and use an explicit C<return> at the end of your rules:
 
  return [201] if $_ eq '/favicon.ico';
  s{^/here(?=/|$)}{/there};
  return;
+
+=item A code reference
+
+Equivalent to the L</C<response>> callback in the new interface, with the same
+arguments and return values.
+
+=item Any other kind of value
+
+Internal rewrite.
+
+=back
+
+=head2 Porting from the old to the new interface
+
+There are two major incompatibilities between the interfaces:
+
+=over 4
+
+=item 1.
+
+You can no longer return status codes as plain scalars, as in C<return 301>.
+You B<must> now C<return [301]> (which you could before, but didn't have to).
+
+=item 2.
+
+Rewriting the response is no longer done by returning a C<sub>.
+Instead you must use the C<response> attribute.
+
+This may be inconvient if the function was closing over variables from the
+C<rules> callback; in that case you now have to explicitly pass that state
+from one callback to the other through the environment hash. However, such
+code is rare, and in all other cases your code will be more readable under
+the new interface.
 
 =back
